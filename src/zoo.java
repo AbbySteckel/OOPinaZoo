65b46ece6be1db75ec544a31a575845971533206
Abigail Steckel <abigailsteckel@students.berkeley.net>
2018-04-16 12:38:26 -0700

    Initial commit

---
+++ src/zoo.java
@@ -0,0 +1,12 @@
+public class zoo {
+    public static void main(String[] args){
+        //zoo z = new zoo();
+        sleep("Tiger");
+        //sleep("Tiger");
+    }
+
+    public static void sleep(String name) {
+        System.out.print(name + " sleeps for 8 hours.");
+    }
+
+}
